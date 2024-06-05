b0d823d4034d1f131ed414d4064e3cd407446852
gungkim <154861338+gungkim@users.noreply.github.com>
2024-06-05 09:12:34 +0900

    플레이어 애니메이션 변경
    상하좌우 걷기 애니메이션 적용. 플레이어 스텟에서 hp추출할 예정.

--- MyProject-Roguelike/Assets/Scripts/Player/Player.cs
+++ MyProject-Roguelike/Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -10,13 +11,29 @@ public class Player : MonoBehaviour
 
     Animator animator;
 
+    private SpriteRenderer spriteRenderer;
 
+    private Rigidbody2D rigid;
+
+    bool isPlayerAlive = true;
+
+    Vector2 inputDirection = Vector2.zero;
+
+    bool isMove = false;
+
+    readonly int InputX_Hash = Animator.StringToHash("InputX");
+    readonly int InputY_Hash = Animator.StringToHash("InputY");
+    readonly int IsMove_Hash = Animator.StringToHash("IsMove");
 
     private void Awake()
     {
-        playerInputActions = GetComponent<PlayerInput>();
+        playerInputActions = new PlayerInput();
 
         animator = GetComponent<Animator>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
@@ -28,23 +45,34 @@ public class Player : MonoBehaviour
     {
         playerInputActions.Player.Enable();
         playerInputActions.Player.Move.performed += OnMove;
-        playerInputActions.Player.Move.canceled += OnMove;
+        playerInputActions.Player.Move.canceled += OnStop;
     }
 
     private void OnDisable()
     {
         playerInputActions.Player.Move.performed -= OnMove;
-        playerInputActions.Player.Move.canceled -= OnMove;
+        playerInputActions.Player.Move.canceled -= OnStop;
         playerInputActions.Player.Disable();
     }
+
+
 
     private void Update()
     {
         
     }
 
-    private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext _)
+    private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
+        inputDirection = context.ReadValue<Vector2>();
+        animator.SetFloat(InputX_Hash, inputDirection.x);
+        animator.SetFloat(InputY_Hash, inputDirection.y);
+        isMove = true;
+        animator.SetBool(IsMove_Hash, isMove);
+    }
 
+    private void OnStop(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ MyProject-Roguelike/Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    public float attackPower = 3.0f;
+
+    public float moveSpeed = 2.0f;
+
+    public float criticalHit = 5.0f;
+
+    public float coolTime = 5.0f;
+
+    public float attackRange = 5.0f;
+
+    public float health = 20.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
